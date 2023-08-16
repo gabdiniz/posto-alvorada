efef113bc92907481c5b42ecb93dcd16faf88fe3
vanessaneves-dev <vanessaneves.dev@gmail.com>
2023-08-15 23:57:55 -0300

    feat alteração no footer web e seção Como chegar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,16 +7,38 @@ export function Footer() {
   return (
     <div className="footer">
       <div className="container">
-        <ul>
-          {/* <Link to="home" spy={true} smooth={true} offset={-100} duration={500}> <FaHome className="icon-navbar" /> Home</Link> */}
-
+        <div>
+          <img
+            className="img-footer"
+            src={logo6}
+            alt="Logo do posto alvorada"
+          />
+        </div>
+        <div>
+          <h2>Localização</h2>
+          <p className="localização">
+            Av. dos Expedicionários Brasileiros, 711 <br /> Bairro Coester,{' '}
+            <br />
+            Fernandópolis - SP
+          </p>
+        </div>
+        <div>
+          <h2>
+            Horário <br /> de <br />
+            Funcionamento
+          </h2>
+          <p>
+            De segunda a domingo, <br /> atendimento 24 horas
+          </p>
+        </div>
+        <div className="redes">
           <a
             className="instagram"
             href="https://www.instagram.com/superpostoalvorada/"
             target="_blank"
             rel="noreferrer"
           >
-            <FaInstagram className="icon-navbar" /> Instagram{' '}
+            <FaInstagram className="icon-footer" /> Instagram{' '}
           </a>
 
           <a
@@ -26,20 +48,13 @@ export function Footer() {
             rel="noreferrer"
           >
             {' '}
-            <FaWhatsapp className="icon-navbar" /> Whatsapp
+            <FaWhatsapp className="icon-footer" /> Whatsapp
           </a>
-        </ul>
-        <div className="img-logo-footer">
-          <img
-            className="img-footer"
-            src={logo6}
-            alt="Logo do posto alvorada"
-          />
         </div>
-        <div className="bottom">
-          <span className="line"></span>
-          <p> SUPER POSTO ALVORADA LTDA - TODOS OS DIREITOS RESERVADOS ©2023</p>
-        </div>
+      </div>
+      <div className="bottom">
+        <span className="line"></span>
+        <p> SUPER POSTO ALVORADA LTDA - TODOS OS DIREITOS RESERVADOS ©2023</p>
       </div>
     </div>
   );
