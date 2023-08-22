61ab5a06816055362814744f9b28f08ffd882d8f
vanessaneves-dev <vanessaneves.dev@gmail.com>
2023-08-22 16:19:52 -0300

    conflito interno ok

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -11,15 +11,9 @@ export function Sobre() {
       <section className="container-sobre">
         <div className="d-flex justify-content-around align-items-center flex-wrap mb-5">
           <img className="logo-sobre" src={img} alt="" />
-<<<<<<< HEAD
           <div className="titulo-sobre">
             <h2 class="fw-bold fs-2">Quem somos</h2>
             <p class="fs-4">
-=======
-          <div>
-            <h2 className="title1 fw-bold fs-2">Quem somos</h2>
-            <p className=" title1 fs-4">
->>>>>>> 7c817458b3434e97eb67a7a2dc60b3bbe712e6d4
               Bem-vindo, o seu destino confiável para abastecimento e <br />
               atendimento excepcional em Fernandópolis.
             </p>
@@ -29,17 +23,10 @@ export function Sobre() {
           <Card style={{ width: '18rem', maxHeight: '30rem' }}>
             <Card.Img variant="top" src={img1} />
             <Card.Body>
-<<<<<<< HEAD
               <Card.Title class="fs-3 fw-bold text-center mb-2">
                 Eficiência de décadas
               </Card.Title>
               <Card.Text class="fs-7 p-2 ms-2">
-=======
-              <Card.Title className="cardColor fs-3 fw-bold text-center mt-4 mb-3">
-                Eficiência de décadas
-              </Card.Title>
-              <Card.Text className="cardColor fs-5 p-2 ms-2">
->>>>>>> 7c817458b3434e97eb67a7a2dc60b3bbe712e6d4
                 Com décadas de história, os orgulhamos em ser uma referência na
                 cidade, oferecendo o melhor produto ao nosso consumidor, que
                 pode sempre confiar no Posto Alvorada.
@@ -49,17 +36,10 @@ export function Sobre() {
           <Card style={{ width: '18rem', maxHeight: '30rem' }}>
             <Card.Img variant="top" src={img3} />
             <Card.Body>
-<<<<<<< HEAD
               <Card.Title class="fs-3 fw-bold text-center mb-2">
                 Excelência em Combustíveis
               </Card.Title>
               <Card.Text class="fs-7 p-2 ms-2 ">
-=======
-              <Card.Title class="cardColor fs-3 fw-bold text-center mt-1 mb-3">
-                Excelência em Combustíveis
-              </Card.Title>
-              <Card.Text className="cardColor fs-5 p-2 ms-2 ">
->>>>>>> 7c817458b3434e97eb67a7a2dc60b3bbe712e6d4
                 Abastecer com confiança é primordial, e é por isso que nos
                 esforçamos incansavelmente para fornecer a mais alta qualidade
                 de gasolina para o seu veículo.
@@ -69,17 +49,10 @@ export function Sobre() {
           <Card style={{ width: '18rem', maxHeight: '30rem' }}>
             <Card.Img variant="top" src={img2} />
             <Card.Body>
-<<<<<<< HEAD
               <Card.Title class="fs-3 fw-bold text-center mb-2">
                 Qualidade de atendimento
               </Card.Title>
               <Card.Text class="fs-7 p-2 ms-2">
-=======
-              <Card.Title className="cardColor fs-3 fw-bold text-center mt-1 mb-3">
-                Qualidade de atendimento
-              </Card.Title>
-              <Card.Text className="cardColor fs-5 p-2 ms-2">
->>>>>>> 7c817458b3434e97eb67a7a2dc60b3bbe712e6d4
                 Nos dedicamos a proporcionar serviços de qualidade, respaldados
                 por uma equipe altamente comprometida que coloca nossos clientes
                 em primeiro lugar.
