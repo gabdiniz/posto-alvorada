4305d281b6d7a91c3e0ca9381669a2cf236521be
TamiresDellaRovere <tamires.rovere@hotmail.com>
2023-09-19 10:24:00 -0300

    estilização marmitaria

--- src/components/Marmitaria/Marmitaria.jsx
+++ src/components/Marmitaria/Marmitaria.jsx
@@ -5,40 +5,45 @@ import './Marmitaria.css';
 
 export function Marmitaria() {
   return (
-    <section className="container-marmitaria">
-      <div className="ms-3 mt-5 pt-5 me-2 text-center">
-        <h2 className="fs-1 mb-5">Marmitaria Alvorada</h2>
-        <p className="fs-4 text-center">
-          Explore uma variedade de sabores frescos e deliciosos a cada dia da
-          semana com nossas marmitas especiais! Oferecemos pratos únicos
-          diariamente, garantindo que cada refeição seja uma experiência
-          incrível. <br /> Disponíveis nos tamanho P e M.
-        </p>
-        <div className="link">
-          <h3>Peça sua marmita no nosso Whatsapp</h3>
+    <div className='marmitaria'>
+      <section className="container-marmitaria">
+
+        <div className="mt-5 text-center">
+          <h2 className="fs-1 mb-4">Marmitaria Alvorada</h2>
+          <p className="fs-4 text-center">
+            Explore uma variedade de sabores frescos e deliciosos a cada dia da
+            semana com nossas marmitas especiais! Oferecemos pratos únicos
+            diariamente, garantindo que cada refeição seja uma experiência
+            incrível. <br /> Disponíveis nos tamanhos P e M.
+          </p>
+
+          <div className="link">
+          <h3>Peça em nosso Whatsapp</h3>
           <Nav.Link
             className="link-whatsapp"
             href="https://api.whatsapp.com/send?phone=5517997248775&text=Ol%C3%A1%2C+gostaria+de+pedir+uma+marmita"
             target="_blank"
             rel="noreferrer"
-          >
-            <FaWhatsapp className="link-whatsapp" />
-          </Nav.Link>
+          />
+          <FaWhatsapp className="link-whatsapp" />
         </div>
-      </div>
-      <div className="imgs-marmita ms-5">
-        <img
-          src={img}
-          alt="Foto do prato com a refeição"
-          style={{ width: '100%', height: 'auto' }}
-        />
-        <img
-          src={img}
-          alt="Foto do prato com a refeição"
-          style={{ width: '100%', height: 'auto' }}
-        />
+        </div>
 
-      </div>
-    </section>
+        <div className="imgs-marmita ms-5">
+          <img
+            src={img}
+            alt="Foto do prato com a refeição"
+          />
+          <img
+            src={img}
+            alt="Foto do prato com a refeição"
+          />
+          <img
+            src={img}
+            alt="Foto do prato com a refeição"
+          />
+        </div>
+      </section>
+    </div>
   );
 }
